f0d332ed13c958dc49cc98cebe1e9696c2a83417
Levtraru <lautarogt94@gmail.com>
2022-07-13 21:58:26 -0300

    Archivos actualizados

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';import { createRoot } from 'react-dom/client';
 import App from './App';
+import Tienda from './components/Tienda/Tienda';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const container = document.getElementById('root');
@@ -8,7 +9,8 @@ const root = createRoot(container);
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/"           element={<App />}         />
+      <Route path="/" element={<App />} />
+      <Route path="tienda" element={<Tienda />} />
     </Routes>
   </BrowserRouter>
 );

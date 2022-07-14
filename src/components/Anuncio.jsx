edbe7d8dfbf7d3cb8405031574a41a6f0c023db5
Levtraru <lautarogt94@gmail.com>
2022-07-13 22:02:37 -0300

    Anuncio de oferta

---
+++ src/components/Anuncio.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: bold;
+`
+
+function Anuncio() {
+  return (
+    <Container>
+      ¡¡IMPERDIBLE!! ¡¡ Con una compra superior a los $ 2000 el envío es Gratis !!
+    </Container>
+  );
+}
+
+export default Anuncio;

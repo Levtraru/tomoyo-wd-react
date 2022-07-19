3fe3ebfbf70c9b270238f1886e16bd71268379e9
Levtraru <lautarogt94@gmail.com>
2022-07-19 20:52:42 -0300

    Chango y Producto

--- src/components/Chango.jsx
+++ src/components/Chango.jsx
@@ -1,5 +1,103 @@
+import styled from "styled-components";
+import Header from "./Header";
+import Extras from './Extras';
+import Footer from './Footer';
+import Anuncio from './Anuncio';
+import FooterTienda from './Tienda/FooterTienda';
+import ChangoProducto from "./ChangoProducto";
+
+const Container = styled.div` padding: 20px; `
+const Titulo = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`
+const TopBoton = styled.button`
+    padding: 10px 20px;
+    border: none;
+    border-radius: 20px;
+    background-color: ${(props)=>props.color};
+    font-style: 20px;
+    color: white;
+    &:hover{
+        background:
+            linear-gradient(rgba(255, 255, 255, 0.45),rgba(255, 255, 255, 0.45)),
+            linear-gradient(${(props)=>props.color}, ${(props)=>props.color});
+    };
+`
+const TopTextos = styled.div``
+const TopTexto = styled.span`
+    text-decoration: underline;
+    cursor: url('https://i.postimg.cc/kgVNCXTC/Cursor2.png'), auto;
+    margin: 0 10px;
+`
+const Bot = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Info = styled.div` flex: 3; `
+
 function Chango () {
-    return (<></>);
+    return (
+        <>
+            <Anuncio />
+            <Extras/>
+            <section className="global">
+                <Header />
+                <Container>
+                    <Titulo>TUS PRODUCTOS</Titulo>
+                    <Top>
+                        <TopBoton color='#d979b6'>SEGUIR COMPRANDO</TopBoton>
+                        <TopTextos>
+                            <TopTexto>CHANGO DE COMPRAS (2)</TopTexto>
+                            <TopTexto>LISTA DE DESEOS</TopTexto>
+                        </TopTextos>
+                        <TopBoton color='teal'>PASAR POR CAJA</TopBoton>
+                    </Top>
+                    <Bot>
+                        <Info>
+                            <ChangoProducto
+                                img='qRPVHt1W/20-Fight.webp'
+                                imgAlt='20-Fight'
+                                producto='Traje de Combate: Fight'
+                                id='120'
+                                color='#EA85A4'
+                                talle='37.5'
+                                precio='$ 30'
+                            />
+                            <ChangoProducto
+                                img='90dS3LRq/45-46-Judgement.webp'
+                                imgAlt='45-46-Judgement'
+                                producto='Traje de Combate: Judgement'
+                                id='1456'
+                                color='#EA85A4'
+                                talle='39'
+                                precio='$ 60'
+                            />
+                            <ChangoProducto
+                                img='52gVQ2tF/6b-Illusion.webp'
+                                imgAlt='6b-Illusion'
+                                producto='Traje de Combate: Illusion'
+                                id='106'
+                                color='yellow'
+                                talle='36'
+                                precio='$ 40'
+                            />
+                            {/* <Resumen>Resumen</Resumen> */}
+                        </Info>
+                    </Bot>
+                </Container>
+                <FooterTienda />
+                <div className="clearfix"></div>
+            </section>
+            <Footer />
+        </>
+    );
 }
 
 export default Chango;
---
+++ src/components/ChangoProducto.jsx
@@ -0,0 +1,75 @@
+import styled from "styled-components"
+import AddCont from "./AddCont"
+
+
+    const Producto = styled.div`
+        display: flex;
+        justify-content: space-between;
+    `
+    const ProdDetalle = styled.div`
+        flex: 2;
+        display: flex;
+    `
+    const Img = styled.img` width: 200px; `
+    const Detalles = styled.div`
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    `
+    const ProdNombre = styled.span``
+    const ProdId = styled.span``
+    const ProdColor = styled.div`
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background-color: ${(props)=>props.color};
+    `
+    const ProdTalle = styled.span``
+    const PrecioDetalle = styled.div`
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    `
+    const ProdPrecio = styled.div`
+        font-size: 30px;
+        font-weight: 300;
+        margin-right: 25px;
+    `
+    const Hr = styled.hr`
+        margin-top: 20px;
+        margin-bottom: 20px;
+        background-color: #EA85A4;
+        border: none;
+        height: 1px;
+    `
+
+function ChangoProducto ({img, imgAlt, producto, id, color, talle, precio}) {
+        return (
+    <>
+            <Producto>
+                <ProdDetalle>
+                    <Img
+                        src={(`https://i.postimg.cc/${img}`)}
+                        alt={(`Foto de ${imgAlt}`)}
+                    />
+                    <Detalles>
+                        <ProdNombre><b>Producto:</b> {producto}</ProdNombre>
+                        <ProdId><b>ID:</b> {id}</ProdId>
+                        <ProdColor color={`${color}`}/>
+                        <ProdTalle><b>Talle:</b> {talle}</ProdTalle>
+                    </Detalles>
+                </ProdDetalle>
+                <PrecioDetalle>
+                    <AddCont />
+                    <ProdPrecio>{precio}</ProdPrecio>
+                </PrecioDetalle>
+            </Producto>
+        <Hr />
+        </>
+        );
+}
+
+export default ChangoProducto;
